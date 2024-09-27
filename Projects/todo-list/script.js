f5e374bf0b8c0115f11fbaf6abc897dfacbc5ec2
Abhijadhav03 <jadhavabhishek53366@gmail.com>
2024-09-27 17:31:27 +0530

    added localstorage

--- Projects/todo-list/script.js
+++ Projects/todo-list/script.js
@@ -1,6 +1,11 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+// Load tasks from localStorage on page load
+window.onload = function () {
+    loadTasks();
+};
+
 function addTask() {
     if (inputBox.value === '') {
         alert("You must write something!");
@@ -9,31 +14,71 @@ function addTask() {
         li.innerHTML = inputBox.value;
 
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7";  
+        span.innerHTML = "\u00d7"; // Unicode for "×" symbol (delete)
         li.appendChild(span);
 
         listContainer.appendChild(li);
-    }
-    inputBox.value = "";  
+        saveTasks(); 
+        inputBox.value = "";  
 
-    saveTaskListeners();
+        saveTaskListeners();
+    }
 }
 
+
 function saveTaskListeners() {
-   
     const listItems = document.querySelectorAll('#list-container li');
     listItems.forEach(item => {
         item.addEventListener('click', function () {
             item.classList.toggle('checked');
+            saveTasks();
         });
     });
 
     const deleteButtons = document.querySelectorAll('#list-container li span');
     deleteButtons.forEach(btn => {
+        
         btn.addEventListener('click', function () {
             btn.parentElement.remove();
+            saveTasks();
         });
     });
+}
+
+
+function saveTasks() {
+    const tasks = [];
+    const listItems = document.querySelectorAll('#list-container li');
+    listItems.forEach(item => {
+        const task = {
+            text: item.firstChild.textContent,
+            isChecked: item.classList.contains('checked')
+        };
+        tasks.push(task);
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks)); 
+}
+
+
+function loadTasks() {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+        const tasks = JSON.parse(savedTasks); 
+        tasks.forEach(task => {
+            let li = document.createElement("li");
+            li.innerHTML = task.text;
+
+            if (task.isChecked) {
+                li.classList.add('checked');   }
+
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7"; 
+            li.appendChild(span);
+
+            listContainer.appendChild(li);
+        });
+        saveTaskListeners(); 
+    }
 }
 
 inputBox.addEventListener("keypress", function (e) {
